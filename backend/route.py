eb20b3cd1d124f1e4619619f5c3147ecdd176726
FelipeDrummond <felipe2002drummond@gmail.com>
2024-05-16 09:17:10 -0300

    gemini

--- backend/route.py
+++ backend/route.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import google.generativeai as genai
+import os
 
 class Image(BaseModel):
     image: str
@@ -10,3 +12,14 @@ app = FastAPI()
 async def create_item(image: Image):
 
     return 'Recebido'
+
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+model = genai.GenerativeModel('gemini-pro')
+
+lista_alimentos= ['carne', 'leite', 'ovo', 'arroz']
+req = ','.join(lista_alimentos)
+
+response = model.generate_content("Write a recipe using the following ingredients {}.".format(req))
+
+
