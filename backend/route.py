2b13267016e5f86c343f98f575c564693e0e674f
thiagohpr <thiagohpr@al.insper.edu.br>
2024-06-03 16:04:51 -0300

    Removing unecessary code

--- backend/route.py
+++ backend/route.py
@@ -8,31 +8,19 @@ from PIL import Image as PILImage
 from pydantic import BaseModel
 import google.generativeai as genai
 import os
-# from tensorflow.keras.layers import Input
-# from tensorflow.keras.models import Model
-# from tensorflow.keras.layers.experimental import TFSavedModelLayer
 
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # This is for development only, specify domains for production
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 model = tf.keras.models.load_model('model/',  compile=False)
-
-# tfsmlayer = tf.keras.layers.TFSMLayer(model_path='model/', call_endpoint='serving_default')
-
-# model_layer = TFSavedModelLayer('model/', call_endpoint='serving_default')
-
-# If you need to incorporate this layer into a larger model:
-# inputs = Input(shape=(input_shape,), dtype=tf.float32)
-# outputs = model_layer(inputs)
-# model = Model(inputs=inputs, outputs=outputs)
 
 labels = {0: 'abacaxi',
  1: 'abobrinha',
@@ -73,7 +61,7 @@ async def upload_image(files: List[UploadFile] = File(...)):
     for file in files:
         image_bytes = await file.read()
         image = PILImage.open(BytesIO(image_bytes))
-        image = image.resize((230, 307))  # Adjust size to match your model's expected input
+        image = image.resize((230, 307))
         image_array = tf.keras.preprocessing.image.img_to_array(image)
         image_array = tf.expand_dims(image_array, 0)
 
